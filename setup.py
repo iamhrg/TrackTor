389cb6c23489ab56bb7c058c308a2b4977e58088
Harsh Gandhi <hrgandhi1@gmail.com>
2019-05-11 18:41:23 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,27 @@
 import setuptools
 from distutils.core import setup
+from setuptools import setup, find_packages
+
 setup(
   name = 'TrackTor',
-  packages = ['TrackTor', 'TrackTor.Home', 'TrackTor.Icons', 'TrackTor.Logs_Data', 'TrackTor.Utilities', 'TrackTor.Logs_Data.Tor', 'TrackTor.Logs_Data.TrackTor'],
-  version = '1.0',,
-  license='GNU General Public License v3 or later (GPLv3+)',
+  packages = ['TrackTor', 'TrackTor.Home', 'TrackTor.Icons', 'TrackTor.Logs_Data', 'TrackTor.Utilities'],
+  version = '1.0',
+  license='MNIT',
   description = 'A Monitoring Tool for TOR Network',
   author = 'Dr. Pilli Emmanuel Shubhakar, Harsh Gandhi, Ritu Karela, Parul Agrawal',
   author_email = 'espilli.cse@mnit.ac.in, hrgandhi1@gmail.com, ritukarela1234@gmail.com, parulagrawal1507@gmail.com',
   url = 'https://github.com/iamhrg/TrackTor',
   keywords = ['TrackTor', 'Tor', 'Deep Web', 'Dark Web', 'Tor Project', 'Security', 'Tracking', 'Monitoring', 'Analysing','Nyx', 'Stem', 'User Friendly', 'UI'],
   install_requires=[
-          'PyQT5',
-          'pyqtgraph',
           'stem',
           'argparse',
+          'collections',
+          'functools',
           'numpy',
+          'sqlite3',
           'pycountry',
           'psutil',
+          'getpass',
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',
